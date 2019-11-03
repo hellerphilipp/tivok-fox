df6e4cb093c923506118ee9205de94bfe3b73f17
Philipp Heller <10487949+hellerphilipp@users.noreply.github.com>
2019-11-03 17:22:05 +0100

    added webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    mode: "development",
+    devtool: "source-map",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
+        ]
+    },
+    // import modules with the following paths from the named global variables instead of bundling them
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
+};

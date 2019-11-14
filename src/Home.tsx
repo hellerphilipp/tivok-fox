ba718a4a07872435727ba2fa43e4abdd6c8a9f49
Philipp Heller <10487949+hellerphilipp@users.noreply.github.com>
2019-11-14 22:10:15 +0100

    added headerbar

--- src/Home.tsx
+++ src/Home.tsx
@@ -13,7 +13,19 @@ export class Home extends React.Component<HomeProps,{}> {
             <div className="h-screen flex">
                 <Sidebar />
                 <div className="flex-1 min-w-0 bg-white">
-                    <div className="border-b-2 border-gray-200"></div>
+                    <div className="border-b-2 border-gray-200">
+                        <header className="flex justify-between items-center px-6 py-3">
+                            <span className="flex items-center">
+                                <h1 className="text-lg">Event details</h1>
+                            </span>
+                            <button>
+                                <img 
+                                    className="h-10 w-10 rounded-full object-cover"
+                                    src="https://lh3.googleusercontent.com/a-/AAuE7mCI1Exy9CMUU99TrafAKVVRLFJnQbf_UIQMc9dX" 
+                                />
+                            </button>
+                        </header>
+                    </div>
                     <div className="overflow-auto"></div>
                 </div>
             </div>

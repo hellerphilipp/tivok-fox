ee38b9bbdcfa66e75d9a743e74bfb37052e444df
Philipp Heller <10487949+hellerphilipp@users.noreply.github.com>
2019-11-14 10:54:00 +0100

    installed tailwindCSS

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,22 @@ module.exports = {
                 enforce: "pre",
                 test: /\.js$/,
                 loader: "source-map-loader"
-            }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    { loader: 'css-loader', options: { importLoaders: 1 } },
+                    { loader: 'postcss-loader', options: {
+                            ident: 'postcss',
+                            plugins: [
+                                require('tailwindcss'),
+                                require('autoprefixer'),
+                            ],
+                        }
+                    }
+                ]
+              }
         ]
     },
     // import modules with the following paths from the named global variables instead of bundling them

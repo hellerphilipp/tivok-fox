cc248e146cf308e9d3ea070daffc3a50b3b51399
Philipp Heller <10487949+hellerphilipp@users.noreply.github.com>
2019-11-23 15:15:26 +0100

    styled login page

--- src/Login.tsx
+++ src/Login.tsx
@@ -8,9 +8,11 @@ interface LoginProps {
 export class Login extends React.Component<LoginProps,{}> {
     render() {
         return (
-            <p>
-                <button onClick={this.props.auth.authorize}>Click to login</button>
-            </p>
+            <div className="flex items-center justify-center w-screen h-screen">
+                <button className="rounded inline-block p-3 font-bold text-white bg-blue-500 hover:bg-blue-700" onClick={this.props.auth.authorize}>
+                    Login or Signup
+                </button>
+            </div>
         )
     }
 }
